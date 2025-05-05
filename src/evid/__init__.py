682cd130c8dc199401cad76c9b51c9de986ebbba
evid <210122431+evidlabel@users.noreply.github.com>
2025-05-05 10:50:16 +0200

    update cli

--- src/evid/__init__.py
+++ src/evid/__init__.py
@@ -1,7 +1,10 @@
 import importlib.metadata
+from pathlib import Path
 
 NAME = "evid"
 try:
     __version__ = importlib.metadata.version(NAME)
 except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
+
+DEFAULT_DIR = Path("~/Documents/evid").expanduser()
