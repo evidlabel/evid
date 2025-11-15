2df74f8597095a831726dfa38626f58803c37f25
evid <210122431+evidlabel@users.noreply.github.com>
2025-11-15 10:01:07 +0100

    Refactor CLI to add one-character alternatives to command line options

--- src/evid/cli/main.py
+++ src/evid/cli/main.py
@@ -122,10 +122,6 @@ set_group.commands.append(
 doc_group = group(
     name="doc",
     help="Manage documents",
-    options=[
-        option(flags=["-s", "--dataset"], arg_type=str, help="Dataset name"),
-        option(flags=["-u", "--uuid"], arg_type=str, help="UUID of the document"),
-    ],
 )
 app.subgroups.append(doc_group)
 
@@ -134,6 +130,10 @@ doc_group.commands.append(
         name="bibtex",
         help="Generate BibTeX files from label.typ files",
         callback=bibtex_callback,
+        options=[
+            option(flags=["-s", "--dataset"], arg_type=str, help="Dataset name"),
+            option(flags=["-u", "--uuid"], arg_type=str, help="UUID of the document"),
+        ],
     )
 )
 
@@ -143,6 +143,8 @@ doc_group.commands.append(
         help="Label a document in a dataset",
         callback=label_callback,
         options=[
+            option(flags=["-s", "--dataset"], arg_type=str, help="Dataset name"),
+            option(flags=["-u", "--uuid"], arg_type=str, help="UUID of the document"),
             option(
                 flags=["-f", "--filename"],
                 arg_type=str,
@@ -158,6 +160,10 @@ doc_group.commands.append(
         name="rebut",
         help="Generate rebuttal for a document in a dataset",
         callback=rebut_callback,
+        options=[
+            option(flags=["-s", "--dataset"], arg_type=str, help="Dataset name"),
+            option(flags=["-u", "--uuid"], arg_type=str, help="UUID of the document"),
+        ],
     )
 )
 
@@ -166,6 +172,10 @@ doc_group.commands.append(
         name="list",
         help="List documents in the dataset",
         callback=list_docs_callback,
+        options=[
+            option(flags=["-s", "--dataset"], arg_type=str, help="Dataset name"),
+            option(flags=["-u", "--uuid"], arg_type=str, help="UUID of the document"),
+        ],
     )
 )
 
